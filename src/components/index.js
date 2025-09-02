db2d0bbff4bd6a74b1fd4df8ff6f2f866b608568
moliya <developper853@gmail.com>
2025-09-02 18:24:26 +0500

    token

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export { default as Navbar } from './navbar'
 export { default as Register } from './register'
 export { default as Login } from './login'
 export { default as ValidationError } from './validation-error'
-=======
-export {default as Navbar} from './navbar'
-export {default as Register} from './register'
-export {default as Login} from './login'
->>>>>>> f3f2589eaaad039a803caead49da5f5b67eaadff
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,6 @@
 import icon from '../Logo/LogoDesktop.png'
 import { Input } from '../ui'
 import { useState } from 'react'
-<<<<<<< HEAD
 import { useDispatch, useSelector } from 'react-redux'
 import { signUserFailure, signUserStart, signUserSuccess } from '../slice/auth'
 import AuthService from '../service/auth'
@@ -13,34 +12,14 @@ const Register = () => {
 	const [password, setPassword] = useState('')
 	const dispatch = useDispatch()
 	const { isLoading } = useSelector(state => state.auth)
-=======
-import { useDispatch,useSelector } from 'react-redux'
-import { signUserFailure,signUserStart,signUserSuccess} from '../slice/auth'	
-import AuthService from '../service/auth'
-
-const Register = () => {
-	const [username,setUsername] = useState('')
-	const [email,setEmail] = useState('')
-	const [password,setPassword] = useState('')
-	const dispatch = useDispatch()
-	const {isLoading} = useSelector(state=>state.auth)
-	
->>>>>>> f3f2589eaaad039a803caead49da5f5b67eaadff
 
 	const registerHandler = async e => {
 		e.preventDefault()
 		dispatch(signUserStart())
-<<<<<<< HEAD
 		const user = { username, email, password }
 		try {
 			const response = await AuthService.userRegister(user)
 			dispatch(signUserSuccess(response.user))
-=======
-		const user ={username,email,password}
-		try {
-			const response = await AuthService.userRegister(user)	
-			dispatch(signUserSuccess(response.user))	
->>>>>>> f3f2589eaaad039a803caead49da5f5b67eaadff
 		} catch (error) {
 			dispatch(signUserFailure(error.response.data.errors))
 		}
@@ -48,7 +27,6 @@ const Register = () => {
 
 	return (
 		<div className='text-center'>
-<<<<<<< HEAD
 			<main className='form-signin w-25  m-auto'>
 				<form>
 					<img
@@ -87,26 +65,9 @@ const Register = () => {
 						{isLoading ? 'Loading...' : 'register'}
 					</button>
 				</form>
-=======
-			<main className="form-signin w-25  m-auto"> 
-				<form> 
-					<img className="mb-4 mt-5" src={icon} alt="icon" width="200" height="60" /> 
-					<h1 className="h3 mb-3 fw-normal">Please sign up</h1>
-					<Input label={"Username"} state={username} setState={setUsername} type={"text"}/>
-					<Input label={"Email address"} state={email} setState={setEmail} type={"email"}/>
-					<Input label={"Password"} state={password} setState={setPassword} type={"password"}/>
-						<button className="btn btn-primary w-100 py-2 my-4" disabled={isLoading} onClick={registerHandler} type="submit">
-						{isLoading ? 'Loading...' : 'register'}
-					</button>  
-				</form> 
->>>>>>> f3f2589eaaad039a803caead49da5f5b67eaadff
 			</main>
 		</div>
 	)
 }
 
-<<<<<<< HEAD
 export default Register
-=======
-export default Register
->>>>>>> f3f2589eaaad039a803caead49da5f5b67eaadff
